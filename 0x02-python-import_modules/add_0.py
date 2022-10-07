5cc70404ed8ad8e27e5556b474790cec9cbe764b
Norbert-OO <norbertodongoo@gmail.com>
2022-10-07 09:20:49 +0000

    My addition function

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+
+    """My addition funtion
+    Args:
+        a: first integer
+        b: Second integer
+
+    Returns: 
+        The value of a + b
+    """
+
+    return (a + b)
