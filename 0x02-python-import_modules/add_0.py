283d6915cd36d8b3e67a2f00ab3e2039039b4275
Norbert-OO <norbertodongoo@gmail.com>
2022-10-07 10:20:23 +0000

    File containing addition function module

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def add(a, b):
 
+
     """My addition funtion
     Args:
         a: first integer
         b: Second integer
 
     Returns: 
-        The value of a + b
+        The value. a + b
     """
 
     return (a + b)
