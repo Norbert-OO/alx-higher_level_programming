be40c6ce2604a80e96d80460278c73e32712eafa
Norbert-OO <norbertodongoo@gmail.com>
2022-10-13 13:28:30 +0000

    File 4 - A function that divides element by element 2 lists.

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,23 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     
-    """A function that divides element by element, 2 lists.
-    - Prototype: def list_division(my_list_1, my_list_2, list_length):
-    - my_list_1 and my_list_2 can contain any type(integer, string, etc.)
-    - list_length (int): The number of elements in each length(can be bigger than the length of botyh lists
-    - You have to use try: / except: / finally:
-    - You are not allowed to import any module
-    
-    Conditions:
-    -If 2 elements can not be divided, the division result should equal to 0
-    - If an element is not an integer or float; print: wrong type
-    - If the division can't be done (/0); print: division by 0
-    - If my_list_1 or my_list_2 is too short; print: out of range
-    Return:
-    A new list(length = list_length) with all divisions
-
-    """
-
     new_list = []
     for i in range(0, list_length):
         try:
