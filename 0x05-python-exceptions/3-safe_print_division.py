eae8a2840d27b0109a0bb061b9e8b345260b72d9
Norbert-OO <norbertodongoo@gmail.com>
2022-10-13 13:22:37 +0000

    File 3 - A function that divides 2 integers and prints the result.

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    """
-    A function that divides 2 integers and prints the result.
-    - Prototype: def safe_print_division(a, b):
-    - You can assume that a and b are integers
-    - The result of the division should print on the finally: section preceded by Inside result:
-    - You have to use try: / except: / finally:
-    - You have to use "{}".format() to print the result.
-    - You are not allowed to import any module.
-
-    Return:
-    The value of the division, Otherwise: None
-
-    """
     result = 0
     try:
         result = a / b
