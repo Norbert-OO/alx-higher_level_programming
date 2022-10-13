c85ccc45974cf374b3a1d62bf15a3b3e67c5adb5
Norbert-OO <norbertodongoo@gmail.com>
2022-10-13 12:59:47 +0000

    File 6 - A function that raises a name exception with a message.

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+    """A function that raises a name exception with a message.
+
+    - prototype: def raise_exception_msg(message=""):
+    - You are not allowed to import any module
+    """
+def raise_exception_msg(message=""):
+    raise NameError(message)
